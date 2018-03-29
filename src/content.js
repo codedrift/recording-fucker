342ebb3e72e140d0a770b095d532bca60cee8a82
Felix Gladisch <felix@felixgladisch.de>
2018-03-29 16:17:19 +0200

    feat: fuck up text and images

--- src/content.js
+++ src/content.js
@@ -1,10 +1,28 @@
-console.log("Hotjar", window.hj)
+console.log("Hotjar", window.hj);
 
-function tiltBody() {
+function fuckUpBody() {
 	document.getElementsByTagName("body")[0].style.setProperty("transform", "rotate(0.003rad)");
 }
 
-if(window.hj) {
-	tiltBody();
+function fuckUpImages() {
+	let index = 0;
+	Array.from(document.getElementsByTagName("img")).forEach(ele => {
+		if (index % 2) {
+			ele.style.setProperty("transform", "translateX(-15px)");
+		}
+		index++;
+	});
 }
-tiltBody();
+
+function fuckUpText() {
+	Array.from(document.getElementsByTagName("p")).forEach(node => {
+		const text = node.innerText;
+		const position = Math.floor(Math.random() * text.length) + 1;
+		const fuckedUpText = text.substring(0, position - 1) + text.substring(position, text.length);
+		node.innerText = fuckedUpText;
+	});
+}
+
+fuckUpText();
+fuckUpImages();
+fuckUpBody();
